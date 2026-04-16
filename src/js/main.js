c8bdbab8a919cc008dba39e8ed380b5534db3ccd
Pontus Johansson <johanssonp998@gmail.com>
2026-04-16 11:20:29 +0200

    lägger till en try & catch i loaddata funktionen

--- src/js/main.js
+++ src/js/main.js
@@ -5,34 +5,39 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 async function loadData() {
-    const res = await fetch(apiUrl);
-    const data = await res.json();
+    try{
+        const res = await fetch(apiUrl);
+        const data = await res.json();
 
-    // Hämtar list-elementet och nollar innehåll
-    const container = document.getElementById("list");
-    container.innerHTML = "";
+        // Hämtar list-elementet och nollar innehåll
+        const container = document.getElementById("list");
+        container.innerHTML = "";
 
-    // Loopar igenom innehåll.
-    data.forEach(item => {
-        const div = document.createElement("div");
-        div.classList.add("item");
+        // Loopar igenom innehåll.
+        data.forEach(item => {
+            const div = document.createElement("div");
+            div.classList.add("item");
 
-        div.innerHTML = `
-            <h3>${item.companyname}</h3>
-            <p>${item.jobtitle} - ${item.location}</p>
-            <p>${item.startdate} - ${item.enddate}</p>
-            <p>${item.description}</p>
-            <button class="delete-btn" data-id="${item.id}">Delete</button>
-        `;
+            div.innerHTML = `
+                <h3>${item.companyname}</h3>
+                <p>${item.jobtitle} - ${item.location}</p>
+                <p>${item.startdate} - ${item.enddate}</p>
+                <p>${item.description}</p>
+                <button class="delete-btn" data-id="${item.id}">Delete</button>
+            `;
 
-        container.appendChild(div);
-    });
-
-    // Event-lyssnare på delete-knappen
-    document.querySelectorAll(".delete-btn").forEach(btn => {
-        btn.addEventListener("click", ()=>{
-            const id = btn.dataset.id;
-            deleteItem(id);
+            container.appendChild(div);
         });
-    });
+
+        // Event-lyssnare på delete-knappen
+        document.querySelectorAll(".delete-btn").forEach(btn => {
+            btn.addEventListener("click", ()=>{
+                const id = btn.dataset.id;
+                deleteItem(id);
+            });
+        });
+        
+    } catch (error){
+        console.error("Fel vi hämtning av data: " + error);
+    }
 }
