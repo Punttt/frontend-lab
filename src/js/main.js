71185e029ec21cbb74222df2b28d3ee3d6a09004
Pontus Johansson <johanssonp998@gmail.com>
2026-04-16 10:36:20 +0200

    testar mig vidare med funktionen

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,23 @@ async function loadData() {
     const res = await fetch(apiUrl);
     const data = await res.json();
 
+    // Hämtar list-elementet och nollar innehåll
     const container = document.getElementById("list");
+    container.innerHTML = "";
+
+    // Loopar igenom innehåll.
+    data.foreEach(item => {
+        const div = document.createElement("div");
+        div.classList.add("item");
+
+        div.innerHTML = `
+            <h3>${item.companyname}</h3>
+            <p>${item.jobtitle} - ${item.location}</p>
+            <p>${item.startdate} - ${item.enddate}</p>
+            <p>${item.description}</p>
+            <button data-id="${item.id}">Delete</button>
+        `;
+
+        container.appendChild(div);
+    });
 }
