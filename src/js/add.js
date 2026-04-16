69631857d4ebef4b4cf67475cda415b0bc717708
Pontus Johansson <johanssonp998@gmail.com>
2026-04-16 12:08:26 +0200

    hämtar form och skapar ett objekt för alla inputs

--- src/js/add.js
+++ src/js/add.js
@@ -1,6 +1,25 @@
 const apiUrl = "http://localhost:3000/api/workexperience";
 
 document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("add-work");
 
-    
+    form.addEventListener("submit", async(e) => {
+        e.preventDefault();
+
+        // Skapar ett objekt för varje input
+        const newItem = {
+            companyname: document.getElementById("companyname").value,
+            jobtitle: document.getElementById("jobtitle").value,
+            location: document.getElementById("location"). value,
+            startdate: document.getElementById("startdate").value,
+            enddate: document.getElementById("enddate").value,
+            description: document.getElementById("description").value
+        };
+
+        try {
+
+        } catch (error) {
+            console.error("Fel vid POST:" + error);
+        }
+    });
 });
